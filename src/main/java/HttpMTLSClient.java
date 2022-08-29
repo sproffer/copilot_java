26b75e50d8128caf568baba65865e523f90ae6c4
sproffer <seek.proffer@gmail.com>
2022-08-29 13:04:10 -0700

    use HttpGet to test perf-api, using local client certificate, pkcs12 format. with TLS1.3

--- src/main/java/HttpMTLSClient.java
+++ src/main/java/HttpMTLSClient.java
@@ -8,6 +8,7 @@ import java.security.KeyStore;
 import org.apache.http.Header;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
 import org.apache.http.client.methods.HttpPost;
 import org.apache.http.config.Registry;
 import org.apache.http.config.RegistryBuilder;
@@ -23,8 +24,8 @@ import org.apache.http.util.EntityUtils;
  * SSL KeyStore is using PKCS12 format.
  */
 public class HttpMTLSClient {
-    String clientKeyStorePassword = null;
-    String clientKeyStorePath = null;
+    String clientKeyStorePassword = "myclient";
+    String clientKeyStorePath = "/Users/garyzhu/mykeystore.pkcs12";
     String serverTrustStorePath = null;
     private static SSLContext sslContext = null;
     private static PoolingHttpClientConnectionManager cm = null;
@@ -50,7 +51,7 @@ public class HttpMTLSClient {
             return null;
         }
         KeyStore clientKeyStore = KeyStore.getInstance(KEYSTORE_TYPE);
-        char[] clientKeyStorePasswordChars = null;
+        clientKeyStorePasswordChars = null;
         if (_clientKeyStorePassword != null) {
             clientKeyStorePasswordChars = _clientKeyStorePassword.toCharArray();
         }
@@ -79,7 +80,7 @@ public class HttpMTLSClient {
         kmf.init(getClientKeyStore(clientKeyStorePath, clientKeyStorePassword), clientKeyStorePasswordChars);
         TrustManagerFactory tmf = TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
         tmf.init(getServerTrustStore(serverTrustStorePath));
-        sslContext = SSLContext.getInstance("TLSv1.2");
+        sslContext = SSLContext.getInstance("TLSv1.3");
         sslContext.init(kmf.getKeyManagers(), tmf.getTrustManagers(), null);
         return sslContext;
     }
@@ -92,7 +93,7 @@ public class HttpMTLSClient {
             return cm;
         }
         SSLContext sslContext = getSSLContext();
-        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, new String[]{"TLSv1.2"}, null,
+        SSLConnectionSocketFactory sslsf = new SSLConnectionSocketFactory(sslContext, new String[]{"TLSv1.3"}, null,
                 SSLConnectionSocketFactory.getDefaultHostnameVerifier());
         Registry<ConnectionSocketFactory> socketFactoryRegistry = RegistryBuilder.<ConnectionSocketFactory>create()
                 .register("https", sslsf)
@@ -149,11 +150,13 @@ public class HttpMTLSClient {
         CloseableHttpClient httpClient = client.getHttpClient();
         // execute an HTTP POST request
         try {
-            HttpPost httpPost = new HttpPost(url);
-            httpPost.addHeader("content-type", "application/json");
-            String postBody = "{\"name\":\"John\"}";
-            httpPost.setEntity(new StringEntity(postBody, StandardCharsets.UTF_8));
-            CloseableHttpResponse response = httpClient.execute(httpPost);
+
+            HttpGet httpGet = new HttpGet(url);
+            //httpPost.addHeader("content-type", "application/json");
+            httpGet.addHeader("authorization", "Basic UkJfU0FMRVNfSFVCOm03emlwYWhraXI=");
+            //String postBody = "{\"name\":\"John\"}";
+            //httpPost.setEntity(new StringEntity(postBody, StandardCharsets.UTF_8));
+            CloseableHttpResponse response = httpClient.execute(httpGet);
             System.out.println("In the middle: connection stats: " + client.getPoolingHttpClientConnectionManager().getTotalStats().toString());
             System.out.println("Response status: " + response.getStatusLine());
 
@@ -162,8 +165,15 @@ public class HttpMTLSClient {
             for (Header header : headers) {
                 System.out.println("    " + header.getName() + ": " + header.getValue());
             }
-
-            System.out.println("Response body: " + EntityUtils.toString(response.getEntity()).substring(0, 128));
+            if (response.getEntity() != null) {
+                // print the first 128 char of the response body, or the entire body if less than 128 char
+                String responseBody = EntityUtils.toString(response.getEntity());
+                if (responseBody.length() > 128) {
+                    System.out.println("Response body: " + responseBody.substring(0, 128) + "...");
+                } else {
+                    System.out.println("Response body: " + responseBody);
+                }
+            }
             response.close();
 
             System.out.println("Connection Manager Stats: " + client.getPoolingHttpClientConnectionManager().getTotalStats().toString());
